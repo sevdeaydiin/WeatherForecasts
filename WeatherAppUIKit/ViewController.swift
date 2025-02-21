2dfc6b283364c198d050b86e4b5e0765c3d57b1e
Sevde Aydın <74006598+sevdeaydiin@users.noreply.github.com>
2025-02-21 16:20:15 +0300

    Initial Commit

---
+++ WeatherAppUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherAppUIKit
+//
+//  Created by Sevde Aydın on 2/21/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
