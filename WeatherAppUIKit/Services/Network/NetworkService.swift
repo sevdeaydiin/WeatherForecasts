8166f055fb3037e50762821f75a4c0c12594e085
Sevde Aydın <74006598+sevdeaydiin@users.noreply.github.com>
2025-02-23 23:39:26 +0300

    feat(service): Add network service

--- WeatherAppUIKit/Services/Network/NetworkService.swift
+++ WeatherAppUIKit/Services/Network/NetworkService.swift
@@ -6,3 +6,70 @@
 //
 
 import Foundation
+import Combine
+
+enum NetworkError: Error {
+    case invalidURL
+    case noData
+    case decodingError
+    case serverError(Int)
+    case unknown
+}
+
+protocol NetworkServiceProtocol {
+    func request<T: Decodable>(_ endpoint: Endpoint) -> AnyPublisher<T, NetworkError>
+}
+
+struct Endpoint {
+    let path: String
+    let queryItems: [URLQueryItem]
+    
+    var url: URL? {
+        var components = URLComponents()
+        components.scheme = "https"
+        components.host = "api.openweathermap.org"
+        components.path = "/data/2.5/\(path)"
+        components.queryItems = [
+            URLQueryItem(name: "appid", value: APIConstants.apiKey),
+            URLQueryItem(name: "units", value: "metric")
+        ] + queryItems
+        
+        return components.url
+    }
+}
+
+final class NetworkService: NetworkServiceProtocol {
+    private let session: URLSession
+    
+    init(session: URLSession = .shared) {
+        self.session = session
+    }
+    
+    func request<T: Decodable>(_ endpoint: Endpoint) -> AnyPublisher<T, NetworkError> {
+        guard let url = endpoint.url else {
+            return Fail(error: NetworkError.invalidURL).eraseToAnyPublisher()
+        }
+        
+        return session.dataTaskPublisher(for: url)
+            .mapError { _ in NetworkError.unknown }
+            .flatMap { data, response -> AnyPublisher<T, NetworkError> in
+                guard let httpResponse = response as? HTTPURLResponse else {
+                    return Fail(error: NetworkError.unknown).eraseToAnyPublisher()
+                }
+                
+                guard (200...299).contains(httpResponse.statusCode) else {
+                    return Fail(error: NetworkError.serverError(httpResponse.statusCode)).eraseToAnyPublisher()
+                }
+                
+                return Just(data)
+                    .decode(type: T.self, decoder: JSONDecoder())
+                    .mapError { _ in NetworkError.decodingError }
+                    .eraseToAnyPublisher()
+            }
+            .eraseToAnyPublisher()
+    }
+}
+
+enum APIConstants {
+    static let apiKey = "API_KEY"
+}
